56909815a058ffdb2e9c5b4040b839f12a4e4bef
Ross Patiak <ross.patiak@gmail.com>
2021-04-26 17:25:53 -0400

    added employee controller model and router

---
+++ Backend/router/employee/employee.router.js
@@ -0,0 +1,16 @@
+let express = require("express");
+let router = express.Router();  //router reference. 
+let EmployeeController = require("../../controller/employee/employee.controller");
+const { check, validationResult } = require('express-validator');
+
+//mapping sub path with http methods. 
+router.post("/addEmployeeDetails",
+[check('EmployeeID').isInt().withMessage('Please enter a ProductID that is a number'),
+check('FistName').isString().withMessage('ProductName should be a string'),
+check('LastName').isString().withMessage('ProductPrice should be a number'),
+check('Password').isString().withMessage('ProductQuantity should be a number')]
+,EmployeeController.addEmployeeDetails);
+
+router.delete("/deleteEmployee/:id",EmployeeController.deleteEmployeeDetails);
+
+module.exports=router;
