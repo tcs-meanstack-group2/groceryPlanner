1d71940ae52c104162fa31d8a0af9bb74c065c78
Ross Patiak <ross.patiak@gmail.com>
2021-04-28 15:44:50 -0400

    created generate reports

--- Frontend/angular-grocer/crud-operations/src/app/app-routing.module.ts
+++ Frontend/angular-grocer/crud-operations/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
+import { EmployeeComponent } from './Components/employee/employee.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
 const routes: Routes = [
   
   { path: 'product-list', component: ProductListComponent },
   { path: 'cart', component: CartComponent },
+  { path: 'employees', component: EmployeeComponent }
 ];
 
 @NgModule({
