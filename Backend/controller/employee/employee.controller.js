8badd6788ef7beff8e2a6c0020d75da558aaa024
Ross Patiak <ross.patiak@gmail.com>
2021-04-28 21:45:20 -0400

    added generate report; fixed add/delete employee

--- Backend/controller/employee/employee.controller.js
+++ Backend/controller/employee/employee.controller.js
@@ -46,52 +46,5 @@ const deleteEmployeeDetails= (req,res)=> {
 
 }
 
-//SHOULD BE ORDERS CONTROLLER
-// const getMonthlyReport = (req, res) => {
-//     const month = req.params.month; //should be number of month
-
-//     // find({ "$expr": { "$eq": [{ "$month": "$timestamp" }, month] } }
-//     OrderModel.find({ $expr: { $eq: [{ $month: $timestamp }, month] } }, (err, result) => {
-//         if(!err){
-//             //returns json of all entries of the specific month
-//             res.json(result);
-//         }
-//       });
-// }
-
-// const getDailyReport = (req, res) => {
-//     const date = req.params.timestamp; //may need convert to date
-
-//     OrderModel.find({timestamp: date}, (err, result) => {
-//         if(!err){
-//             //returns json of all entries of the specific day
-//             res.json(result);
-//         }
-//       });
-// }
-
-// const specificUserReport = (req, res) => {
-//     const id = req.params.id; 
-
-//     OrderModel.find({ userID: id }, (err, result) => {
-//         if(!err){
-//             //returns json of all entries of the specific day
-//             res.json(result);
-//         }
-//       });
-// }
-
-// //might be chalked; need extra table for products ordered
-// const specificProductReport = (req, res) => {
-//     const id = req.params.id; 
-
-//     OrderModel.find({ userID: id }, (err, result) => {
-//         if(!err){
-//             //returns json of all entries of the specific day
-//             res.json(result);
-//         }
-//       });
-// }
-
 
 module.exports={addEmployeeDetails, deleteEmployeeDetails, getEmployeeDetails}
