62bb058d7182ed731a72ef7c204c72562c28e43c
Amir Ali <amir.a.ali071@gmail.com>
2021-04-29 09:32:30 -0500

    Fixed routing errors

--- Frontend/angular-grocer/crud-operations/src/app/Services/admin.service.ts
+++ Frontend/angular-grocer/crud-operations/src/app/Services/admin.service.ts
@@ -14,24 +14,24 @@ export class AdminService {
 
   signIn(AdminId:string,Password:string){
     console.log(AdminId+Password);
-    return this.http.post<{token:string}>('http://localhost:9090/admin/adminSignIn',{AdminId,Password});
+    return this.http.post<{token:string}>(this.ipAddress + '/admin/adminSignIn',{AdminId,Password});
   };
   
   storeProductDetailsInfo(productRef:any){
-    this.http.post('http://localhost:9090/admin/product/addProductDetails',productRef,{responseType:"text"}).
+    this.http.post(this.ipAddress + '/admin/product/addProductDetails',productRef,{responseType:"text"}).
     subscribe(result=>console.log(result),error=>console.log(error));
   }
   
   retrieveAllProductDetails():Observable<Product[]>{
-    return this.http.get<Product[]>('http://localhost:9090/admin/product/allProductDetails');
+    return this.http.get<Product[]>(this.ipAddress + '/admin/product/allProductDetails');
  }
   
  updateProductDetails(productRef:any):any{
-  return this.http.put('http://localhost:9090/admin/product/updateProductDetails',productRef,{responseType:'text'})
+  return this.http.put(this.ipAddress + '/admin/product/updateProductDetails',productRef,{responseType:'text'})
 }
 
 deleteProductById(id:any):any{
-  return this.http.delete('http://localhost:9090/admin/product/deleteProductDetailsById/'+id,{responseType:'text'});
+  return this.http.delete(this.ipAddress + '/admin/product/deleteProductDetailsById/'+id,{responseType:'text'});
 }
 
 generateDailyReport(date:any):Observable<Order[]>{
